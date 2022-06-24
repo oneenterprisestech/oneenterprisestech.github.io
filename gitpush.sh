19b47f3c1d083488036c2d259648ec812c8927a7
Logan Alldredge <one-enterprises.tech@outlook.com>
2022-06-24 09:23:39 -0600

    548

--- gitpush.sh
+++ gitpush.sh
@@ -8,6 +8,16 @@ case $yn in
 [Nn]* ) echo "Stopping the program"; exit;;
 esac
 
+echo "Your main branch will be shown below."
+
+git remote show origin | sed -n '/HEAD branch/s/.*: //p'
+
+read -p "What branch would you like to write changes to? For example, enter master for main/default branch. " BRANCH
+
+echo "The number of commits for branch" $BRANCH "will show below."
+
+git rev-list --count $BRANCH
+
 git add .
 
 read -r -p "Commit name/number/identifier: " NUM
